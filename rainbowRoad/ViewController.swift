4f4a2fb4392214672278f24f8da29d5a2b0a974a
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-11-12 11:45:45 -0800

    first commit

--- rainbowRoad/ViewController.swift
+++ rainbowRoad/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,3 +25,16 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
